ab09e2b173a44660b0ce096da341bfaa737797bc
song21298 <577164601@qq.com>
2018-03-22 19:25:01 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/firstsqlite/sqlitedemo/mylibrary/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.firstsqlite.sqlitedemo.mylibrary;
+
+import android.app.Activity;
+
+/**
+ * 创建日期：2018/3/22
+ * 创建人：SXW
+ * 描述：
+ * 备注：
+ */
+
+public class BaseActivity extends Activity {
+
+}
